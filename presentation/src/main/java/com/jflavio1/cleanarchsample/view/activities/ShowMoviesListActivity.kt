4435d4d9e7202a64f0462a82d6bbc2d3dd7b6706
JoseFlavio <jflavio90@gmail.com>
2018-03-14 21:46:33 -0500

    added package structure to thre layers

--- presentation/src/main/java/com/jflavio1/cleanarchsample/view/activities/ShowMoviesListActivity.kt
+++ presentation/src/main/java/com/jflavio1/cleanarchsample/view/activities/ShowMoviesListActivity.kt
@@ -1,9 +1,10 @@
-package com.jflavio1.cleanarchsample
+package com.jflavio1.cleanarchsample.view.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jflavio1.cleanarchsample.R
 
-class ShowMoviesList : AppCompatActivity() {
+class ShowMoviesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
