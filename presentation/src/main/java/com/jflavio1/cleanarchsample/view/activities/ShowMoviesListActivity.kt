987560f7758609871808b5ed8e445961d3868eca
JoseFlavio <jflavio90@gmail.com>
2018-03-14 21:51:10 -0500

    added author to show movies activity

--- presentation/src/main/java/com/jflavio1/cleanarchsample/view/activities/ShowMoviesListActivity.kt
+++ presentation/src/main/java/com/jflavio1/cleanarchsample/view/activities/ShowMoviesListActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.jflavio1.cleanarchsample.R
 
+/**
+ * @author Jose Flavio
+ */
 class ShowMoviesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
